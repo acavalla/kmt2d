4953617b8da853a75987f632f54df92903de4a67
Anna Cavalla <acavalla@users.noreply.github.com>
2018-04-06 16:18:57 -0700

    Create cat_maf.R

---
+++ R/cat_maf.R
@@ -0,0 +1,41 @@
+x <- read.table("/projects/acavalla_prj/kmt2d/301067/patient_data/summarybysample.txt", sep = "\t", fill = TRUE, stringsAsFactors = FALSE, header = TRUE)
+
+a <- c("BL_ICGC_MALY-DE_orig", "BL_Love_Dave_2012", "CLL", "batch7",
+       "DLBCL_ICGC", "DLBCL_TCGA", "FL_Connors",
+       "FL_Shah", "MCL_Bea_Campo_2013", "MCL_Sweden", "MCL_Zhang_Dave_2014")
+
+ff <- list()
+for(i in 1:length(a)){
+    ff[[i]] <- x[grepl(a[[i]], x[,4]),]
+  }
+
+wt <- list()
+mut <- list()
+for(i in 1:length(ff)){
+  mut[[i]] <- ff[[i]][ff[[i]]$mut=="MUT",]
+  wt[[i]] <- ff[[i]][ff[[i]]$mut=="WT",]
+}
+
+z <- list(wt,mut)
+
+for(h in 1:length(z)){
+  y <- z[[h]]
+  for(i in 1:length(y)){
+    y[[i]] <- paste(y[[i]]$mafsnv, y[[i]]$mafindel)
+  }
+  if(h==1){
+    wt <- y
+  } else {
+    mut <- y
+  }
+}
+
+z <- list(wt,mut)
+
+for(h in 1:length(z)){
+  y <- z[[h]]
+  for(i in 1:length(y)){
+    for (j in 1:length(y[[i]])) {cat(y[[i]][j], "")}
+    cat("\n")
+  }
+}
